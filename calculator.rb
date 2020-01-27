5f25042e56196592b3f0a17c1377b1a9e39d5a86
maxathome <maxim.levine@gmail.com>
2020-01-27 13:21:33 -0500

    First commit

---
+++ calculator.rb
@@ -0,0 +1,55 @@
+require_relative 'ether_prices'
+
+$percent_diff = 10
+$write_ping_history = File.open('ping_history.txt', 'w+')
+$read_ping_history = File.open('ping_history.txt', 'r')
+# $write_ping_history.puts Time.now.utc
+# $write_ping_history.close
+# puts $read_ping_history.read
+
+
+def percent_difference()
+  price_now = get_price_now
+  price_24hr = get_price_24hr
+  puts "Price Now: " + price_now.to_s
+  puts "Price 24 Hours Ago: " + price_24hr.to_s
+  difference = (price_now - price_24hr)
+  average = (price_now + price_24hr) / 2
+  unrounded_percent_diff = (difference.abs() / average)* 100
+  $percent_diff = unrounded_percent_diff.round(1)
+  # puts "24h change is " + $percent_diff.round(1).to_s + "%"
+end
+
+# percent_difference()
+
+
+def send_or_not()
+  loop do
+  if File.readlines("ping_history.txt").grep(/monitor/).size == 0 
+     $write_ping_history.puts Time.now.utc
+     $write_ping_history.close
+     puts "This is the file check: " + $read_ping_history.read
+    
+
+  elsif $read_ping_history.read < (Time.now.utc - 86400).to_s 
+     puts "I'm not gonna write anything because I did less than 24 hours ago"
+     break
+  elsif $percent_diff >= 10
+     puts 'Boys, Ether prices are changing fast. Up by '+ $percent_diff.to_s + ' in last 24 hours.'
+     $write_ping_history.puts Time.now.utc
+     # $write_ping_history.close
+     break
+  else
+     puts 'Ether is stable, boys.'
+     break
+  end
+  end
+end
+
+
+send_or_not
+
+# read_ping_history = File.open('ping_history.txt', &:gets)
+# puts read_ping_history.read
+
+
---
+++ ether_prices.rb
@@ -0,0 +1,23 @@
+require 'net/http'
+require 'json'
+require 'dotenv/load'
+
+
+def get_price_now()
+  url = ENV['GET_PRICE_URL'] + ENV['CYRPTOCOMPARE_API_KEY']
+  uri = URI(url)
+  response = Net::HTTP.get(uri)
+  value = JSON.parse(response)
+  value["USD"]
+end
+
+def get_price_24hr()
+  url = ENV['GET_PRICE_24HR_URL'] + ENV['CYRPTOCOMPARE_API_KEY']
+  uri = URI(url)
+  response = Net::HTTP.get(uri)
+  value = JSON.parse(response)
+  value["Data"]["Data"][0]["close"]
+end
+
+
+  
