5c832997b86fc7de813efb756552737e5ddd4ca1
maxathome <maxim.levine@gmail.com>
2020-01-27 22:06:23 -0500

    change send_or_not method name and create send flag

--- calculator.rb
+++ calculator.rb
@@ -3,7 +3,7 @@ require 'time'
 
 $percent_diff = 0
 $read_ping_history = File.open('ping_history.txt', 'r')
-
+$send_flag = false 
 
 def percent_difference(current_price, past_price)
   puts "Price Now: " + current_price.to_s
@@ -15,7 +15,7 @@ def percent_difference(current_price, past_price)
   puts "Change is " + $percent_diff.round(1).to_s + "%"
 end
 
-def send_or_not
+def set_send_flag
   if File.zero?($read_ping_history) 
     File.write('ping_history.txt', (Time.now.utc - 86400))
   elsif Time.parse(File.read('ping_history.txt')) - (Time.now.utc - 86400) >= 86000 
@@ -23,10 +23,12 @@ def send_or_not
   elsif $percent_diff >= 10
     puts 'Boys, Ether prices are changing fast. Up by '+ $percent_diff.to_s + ' in last 24 hours.'
     File.write('ping_history.txt', (Time.now.utc))
+    $send_flag = true
   else
     puts 'Ether is stable, boys.' 
   end
 end
 
 percent_difference(get_price_now, get_price_24hr) 
-send_or_not
+set_send_flag
+
