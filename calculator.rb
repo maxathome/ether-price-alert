a833154ae43542d7a1a625f8f2b76616c57e29f0
maxathome <maxim.levine@gmail.com>
2020-02-06 13:54:43 -0500

    fix last commit

--- calculator.rb
+++ calculator.rb
@@ -9,16 +9,8 @@ $percent_diff = get_price_24hr.round(1)
 $price_now = get_price_now
 $send_flag = false 
 
-def percent_difference(current_price, past_price)
-  puts "Price Now: " + current_price.to_s
-  puts "Past Price: " + past_price.to_s
-  $price_now = current_price 
-  difference = (current_price - past_price)
-  average = (current_price + past_price) / 2
-  unrounded_percent_diff = (difference.abs() / average)* 100
-  $percent_diff = unrounded_percent_diff.round(1)
-  puts "Change is " + $percent_diff.round(1).to_s + "%"
-end
+puts "Price Now: " + $price_now.to_s
+puts "Change is " + $percent_diff.to_s + "%"
 
 def set_send_flag
   if $redis.get("lastrun") == "true"   
@@ -38,6 +30,5 @@ def post_if_flag_set
   end
 end
 
-# percent_difference(get_price_now, get_price_24hr) 
 set_send_flag
 post_if_flag_set  
